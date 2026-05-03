8d218bcea0799648a403976e7f1d25d9e50f4442
Dharmendra Chauhan <dmma359800@gmail.com>
2026-05-03 17:07:54 +0530

    feat: add getWatchHistory endpoint to retrieve user's watch history with video and owner details

--- Backend_Work/src/controllers/user.controller.js
+++ Backend_Work/src/controllers/user.controller.js
@@ -564,6 +564,60 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
   )
 })
 
+// Here using subpipeline
+const getWatchHistory = asyncHandler (async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id) // yha id ko convert karna padta hai kyuki aggregate apna method sab lagata hai conversion neeeded
+      }
+    }, 
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory", // other pipeline
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    },
+    { // for front-end
+      $addFields: {
+        owner: {
+          $first: "$owner"
+        }
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      user[0].watchHistory,
+      "Watch history fetched succesfully"
+    )
+  )
+})
 
 
 export {
@@ -577,4 +631,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory
 }
