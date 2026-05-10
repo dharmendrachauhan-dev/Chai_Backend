8f90dde35261d6e603ba5de704ac24bfe5d8f784
Dharmendra Chauhan <dmma359800@gmail.com>
2026-05-10 15:07:50 +0530

    enhance comment functionality with validation, error handling, and pagination for video comments

--- Backend_Work/src/controllers/comment.controller.js
+++ Backend_Work/src/controllers/comment.controller.js
@@ -6,7 +6,7 @@ import { Comment } from "../models/comment.model.js";
 
 
 
-const addComment = asyncHandler(async(req, res) => {
+const addComment = asyncHandler(async (req, res) => {
     // Create comments here
     // TODO
     // get id of video, owner and take content from req.body
@@ -21,15 +21,15 @@ const addComment = asyncHandler(async(req, res) => {
     const { content } = req.body
     const userId = req.user._id //coming from middleware
 
-    if(!mongoose.Types.ObjectId.isValid(videoId)){
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
         throw new ApiError(400, "Video ID invalid")
     }
 
-    if(!content || content.trim() === ""){
+    if (!content || content.trim() === "") {
         throw new ApiError(400, "Comment not found")
     }
 
-    if(content.length >= 300){
+    if (content.length >= 300) {
         throw new ApiError(400, "Character excided the limit")
     }
 
@@ -39,22 +39,22 @@ const addComment = asyncHandler(async(req, res) => {
         owner: userId
     })
 
-    if(!comment){
+    if (!comment) {
         throw new ApiError(400, "Something went wrong while creating comment")
     }
 
     return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            comment,
-            "Comment successfully created."
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                comment,
+                "Comment successfully created."
+            )
         )
-    )
 })
 
-const updateComment = asyncHandler( async (req, res) => {
+const updateComment = asyncHandler(async (req, res) => {
     //TODO
     // step-1 take commentId=> req.param and content=> req.body ✅
     // step-2 to commentid ko validate ✅
@@ -68,46 +68,46 @@ const updateComment = asyncHandler( async (req, res) => {
     const { commentId } = req.params
     const { content } = req.body
 
-    if(!mongoose.Types.ObjectId.isValid(commentId)){
+    if (!mongoose.Types.ObjectId.isValid(commentId)) {
         throw new ApiError(400, "CommentId not valid")
     }
 
-    if(!content || content.trim() === ""){
+    if (!content || content.trim() === "") {
         throw new ApiError(400, "Comment not Found")
     }
 
     const comment = await Comment.findById(commentId)
 
-    if(!comment){
+    if (!comment) {
         throw new ApiError(400, "Comment not found")
     }
 
-    if(comment.owner.toString() !== req.user._id.toString()){
+    if (comment.owner.toString() !== req.user._id.toString()) {
         throw new ApiError(403, "Unauthorised action")
     }
 
     const updateComment = await Comment.findByIdAndUpdate(
-        {_id: commentId},
+        { _id: commentId },
         {
             $set: {
                 content
             }
         },
-        { new : true}
+        { new: true }
     )
 
     return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            updateComment,
-            "Comment successfully updated"
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                updateComment,
+                "Comment successfully updated"
+            )
         )
-    )
 })
 
-const deleteComment = asyncHandler( async (req, res) => {
+const deleteComment = asyncHandler(async (req, res) => {
     // delete comment
     // get content and commentId
     // check both
@@ -119,37 +119,118 @@ const deleteComment = asyncHandler( async (req, res) => {
 
     const { commentId } = req.params
 
-    if(!mongoose.Types.ObjectId.isValid(commentId)){
+    if (!mongoose.Types.ObjectId.isValid(commentId)) {
         throw new ApiError(400, "Invalid ID")
     }
 
     const comment = await Comment.findById(commentId)
-    if(!comment){
+    if (!comment) {
         throw new ApiError(400, "Comment not found")
     }
 
-    if(comment.owner.toString() !== req.user._id.toString()){
+    if (comment.owner.toString() !== req.user._id.toString()) {
         throw new ApiError(403, "Unauthorized Action")
     }
 
     await Comment.findByIdAndDelete(commentId) // ✅ simpler than findOneAndDelete
 
     return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            null,
-            "Comment succefully deleted."
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                null,
+                "Comment succefully deleted."
+            )
         )
-    )
-} )
-
-const getVideoComment = asyncHandler ( async (req, res) => {
-    // get all comments
 })
 
-export{
+const getVideoComment = asyncHandler(async (req, res) => {
+    // get all comments for a video
+    // TODO
+    // Get videoId from req.params
+    // Get page and limit from req.query
+    // page: which page user is on => (default => 1)
+    // limit: how many commnets per page => (default => 10)
+    // validate videoId
+    // check video exits in video collection by find method
+    // build aggregate pipeline
+    // => $match
+    // => $lookup
+    // => $project
+    // use mongooseAggregatePaginate
+    // => pass page and limit here
+    // => in handles skipping automatically
+    // check if commnets found
+    // return response
+
+    const { videoId } = req.params
+    const { page = 1, limit = 10 } = req.query
+
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "Video ID not found")
+    }
+
+    const videoComments = await Comment.find({ video: videoId })
+    if (!videoComments) {
+        throw new ApiError(400, "Video comment not found")
+    }
+
+    const commentsAggregate = Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId) // converting bzc params mei string mei milta hai
+            }
+        },
+        {
+            $lookup: {
+                from: "users", // go to the users collection
+                localField: "owner",   // take owner ID from the comment
+                foreignField: "_id",  // match with _id in users
+                as: "owner",   // attach result back as owner
+                pipeline: [
+                    {
+                        $project: {  // only brings this fields
+                            username: 1, //user field
+                            fullName: 1, //user field
+                            avatar: 1 //user field
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                owner: {"$first": owner}  // array to object
+            }
+        }
+    ])
+
+    const getComments = await Comment.aggregatePaginate(
+        commentsAggregate,
+        {
+            page: parseInt(page), // convert str to num
+            limit: parseInt(limit) // convert str to num
+        }
+    )
+
+    if (!getComments) {
+        throw new ApiError(400, "All Comments not found")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                getComments,
+                "Video comment succefully fetched"
+            )
+        )
+
+})
+
+export {
     addComment,
     updateComment,
     deleteComment,
