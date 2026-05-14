bf64b0f49d8330e7ecc9a6933ba346b11a5a6a78
Dharmendra Chauhan <dmma359800@gmail.com>
2026-05-14 14:01:32 +0530

    import Video model in playlist.controller.js for playlist creation functionality

--- Backend_Work/src/controllers/playlist.controller.js
+++ Backend_Work/src/controllers/playlist.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { Playlist } from "../models/playlist.model.js"
-import { jsx } from "react/jsx-runtime"
+import { Video } from "../models/video.model.js"
 
 const createPlaylist = asyncHandler(async (req, res) => {
     // TODO 
